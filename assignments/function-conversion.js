1aa048030aeb6d4c1091ff2bd74c0c4635f16189
Joseph Graham <Joseph@Chasingthefiks.net>
2019-10-14 16:25:45 -0600

    MVP Met All Criteria

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,25 +4,30 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
-
+let myFunction = () => { console.log("Function was invoked!"); }
+myFunction();
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
-
+let anotherFunction = (param1) => (param1);
+console.log(anotherFunction("test"));
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
-
+let add = (param1, param2) => (param1 + param2);
+console.log(add(3, 1));
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
-
+let subtract = (param1, param2) => (param1 - param2);
+console.log(subtract(9, 1));
 // Stretch
-
+const exampleArray = [1, 2, 3, 4];
+const tripple = (exampleArray) => (exampleArray.map(function(num) { return num * 3; }));
+console.log(tripple);
 // exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
